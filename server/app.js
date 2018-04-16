694bd71a582281446fc69d7327e008df8527b996
Rafaela9136 <rafaela9136@gmail.com>
2018-04-15 23:45:05 -0300

    cors module

--- server/app.js
+++ server/app.js
@@ -28,16 +28,7 @@ app.use(express.static(path.join(__dirname, '../client')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
-// Add headers
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-  next();
-});
-
+app.use(cors())
 
 //-------------------------------------------------------------------------
 app.get('/', function(req, res) {
