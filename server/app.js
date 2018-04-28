c33b5b3d30f8243ae294fda592d33967779f0473
Rafaela Lacerda de Araújo <rafaela9136@gmail.com>
2018-04-28 19:41:38 -0300

    extend url

--- server/app.js
+++ server/app.js
@@ -29,7 +29,10 @@ var app = express();
 app.use(express.static(path.join(__dirname, '../client')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: true,
+    limit: '50mb'
+}));
 app.use(cors());
 
 //-------------------------------------------------------------------------
