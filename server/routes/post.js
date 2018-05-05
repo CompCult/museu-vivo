9d0b8de8bb250e5c26d8fab321c8fd995837e8e4
Rafaela9136 <rafaela9136@gmail.com>
2018-05-05 18:55:27 -0300

    update posts: change put to post

--- server/routes/post.js
+++ server/routes/post.js
@@ -44,29 +44,33 @@ router.post('/', function(req, res) {
 });
 
 // Update
-router.put('/:post_id', function(req, res) {
+router.post('/update/:post_id', function(req, res) {
   Post.findById(req.params.post_id, function(err, post) {
-    if (req.body.text_msg) post.text_msg       = req.body.text_msg;
-    if (req.body.picture) {
-      var date = new Date();
-      var timeStamp = date.toLocaleString(); 
-      Uploads.uploadFile(req.body.picture, req.body._user.toString(), timeStamp);
-
-      var filename = req.body._user.toString() + timeStamp + '.jpg'; 
-      post.picture = 'https://s3.amazonaws.com/compcult/minhaarvore/' + filename;
-    }
-    if (req.body.audio) post.audio             = req.body.audio;
-    if (req.body.video) post.video             = req.body.video;
-    if (req.body.location_lat) post.location_lat = req.body.location_lat;
-    if (req.body.location_lng) post.location_lng = req.body.location_lng;
+    if (!post || err) {
+      res.status(400).send('Post não encontrado!');
+    } else {
+        if (req.body.text_msg) post.text_msg       = req.body.text_msg;
+        if (req.body.picture) {
+          var date = new Date();
+          var timeStamp = date.toLocaleString(); 
+          Uploads.uploadFile(req.body.picture, req.body._user.toString(), timeStamp);
     
-    post.save(function(err) {
-      if (err) {
-        res.status(400).send(err);
-      } else {
-        res.status(200).send(post._id);
+          var filename = req.body._user.toString() + timeStamp + '.jpg'; 
+          post.picture = 'https://s3.amazonaws.com/compcult/minhaarvore/' + filename;
+        }
+        if (req.body.audio) post.audio             = req.body.audio;
+        if (req.body.video) post.video             = req.body.video;
+        if (req.body.location_lat) post.location_lat = req.body.location_lat;
+        if (req.body.location_lng) post.location_lng = req.body.location_lng;
+        
+        post.save(function(err) {
+          if (err) {
+            res.status(400).send(err);
+          } else {
+            res.status(200).send(post);
+          }
+        });
       }
-    });
   });
 });
 
