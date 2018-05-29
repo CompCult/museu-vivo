2e396e7fcd2aa388f315a8b3cff656cdb631292c
Rafaela9136 <rafaela9136@gmail.com>
2018-05-28 22:46:00 -0300

    try

--- server/routes/user.js
+++ server/routes/user.js
@@ -99,14 +99,13 @@ router.get('/recovery/password_edit', function(req, res) {
 router.post('/recovery', function(req, res) {
   let user_email = req.body.email;
   let new_password = req.body.new_password;
-  //http://minha-arvore.herokuapp.com
   let html = "<div style='width:90%; margin-left:auto; margin-right:auto; margin-bottom: 20px; border: 1px solid transparent; border-radius: 4px;'>" +
             "<div style='font-family: Arial; border-color: #502274;'>" +
             "<div style='vertical-align:middle; text-align:justify;'>" +
             "<p style='text-align:left;'>Olá!</p>" + 
             "<p>Você está recebendo esse e-mail que foi requisitada a alteração da sua senha de acesso. Se você não fez nenhuma requisição, pode simplesmente ignorar este e-mail.</p>" +
               "<p>Para confirmar a alteração da senha, clique no botão abaixo:</p>" + 
-                "<form action ='https://minha-arvore.herokuapp.com/users/recovery/password_edit?email=" + req.body.email + "' method='post'>" +
+                "<form action ='http://minha-arvore.herokuapp.com/users/recovery/password_edit?email=" + req.body.email + "' method='post'>" +
                 "<input type='submit' value='Confirmar alteração de senha' style='margin-top:3px; margin-bottom:3px; background: #502274; margin-bottom: 3px; padding: 10px; text-align: center; color: white; font-weight: bold; border: 1px solid #502274;'></form>" +
                 "<p style='text-align:left;' >Bom uso,</p>" + 
                 "<p style='text-align:left;' ><b>Equipe Minha Árvore!</b></p>" + 
